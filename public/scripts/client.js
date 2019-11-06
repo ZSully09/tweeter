1aa4c4d33627f78a1040d386618fef0929e56a3a
ZSully09 <zsullivan93@gmail.com>
2019-11-06 23:06:46 +0000

    implemented createTweetElement function and renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,20 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+
+// const tweetData = {
+//   user: {
+//     name: 'Newton',
+//     avatars: 'https://i.imgur.com/73hZDYK.png',
+//     handle: '@SirIsaac'
+//   },
+//   content: {
+//     text: 'If I have seen further it is by standing on the shoulders of giants'
+//   },
+//   created_at: 1461116232227
+// };
+
 const data = [
   {
     user: {
@@ -31,15 +45,84 @@ const data = [
 ];
 
 const renderTweets = function(tweets) {
+  const $tweetContainer = $('.tweet-container');
   // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
+  tweets.forEach(tweet => {
+    // calls createTweetElement for each tweet
+    console.log(createTweetElement(tweet));
+    const $tweet = createTweetElement(tweet);
+    $tweetContainer.append($tweet);
+  });
 };
 
+/* <article class="tweet">
+<header>
+  <p class="avatar-name">
+    <img src="/images/me.jpg" /> Zach Sullivan
+  </p>
+  <p class="handle">@ZSully09</p>
+</header>
+<div class="content">
+  BRAAAAP
+</div>
+<footer>
+  <p class="timestamp">10 days ago</p>
+  <p class="commands">
+    <i class="fa fa-flag-o"></i>
+    <i class="fa fa-retweet"></i>
+    <i class="fa fa-heart-o"></i>
+  </p>
+</footer>
+</article> */
 const createTweetElement = function(tweet) {
-  let $tweet = $('<article>').addClass('tweet');
-  // ...
-  return $tweet;
+  const $img = $('<img>').attr('src', tweet.user.avatars);
+
+  const $pAvatarName = $('<p>')
+    .addClass('avatar-name')
+    .text(tweet.user.name)
+    .append($img);
+
+  const $pHandle = $('<p>')
+    .addClass('handle')
+    .text(tweet.user.handle);
+
+  const $contentDiv = $('<div>')
+    .addClass('content')
+    .text(tweet.content.text);
+
+  const $timestamp = $('<p>')
+    .addClass('timestamp')
+    .text(tweet.created_at);
+
+  const $pCommands = $('<p>')
+    .addClass('commands')
+    .html(
+      '<i class="fa fa-flag-o"></i> <i class="fa fa-retweet"></i> <i class="fa fa-heart-o"></i>'
+    );
+
+  const $header = $('<header>')
+    .append($pAvatarName)
+    .append($pHandle);
+
+  const $footer = $('<footer>')
+    .append($timestamp)
+    .append($pCommands);
+
+  const $article = $('<article>').addClass('tweet');
+
+  return $article
+    .append($header)
+    .append($contentDiv)
+    .append($footer);
 };
 
-renderTweets(data);
+$(document).ready(function() {
+  renderTweets(data);
+});
+
+// $(document).ready(function() {
+//   const $tweet = createTweetElement(tweetData);
+//   // Test / driver code (temporary)
+//   console.log($tweet); // to see what it looks like
+//   $('.tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+// });
