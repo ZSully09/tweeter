4ae03bd796364ce812f112f46c27e33f53d98054
ZSully09 <zsullivan93@gmail.com>
2019-11-07 02:41:59 +0000

    log testing

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,15 +1,15 @@
-"use strict";
+'use strict';
 
 // Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
+const simulateDelay = require('./util/simulate-delay');
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
-
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
       simulateDelay(() => {
+        console.log('inside delay', newTweet);
         db.tweets.push(newTweet);
         callback(null, true);
       });
@@ -22,6 +22,5 @@ module.exports = function makeDataHelpers(db) {
         callback(null, db.tweets.sort(sortNewestFirst));
       });
     }
-
   };
-}
+};
