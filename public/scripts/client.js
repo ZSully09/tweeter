f4ecc486edb4059620ace0bd58b7608533aee27e
ZSully09 <zsullivan93@gmail.com>
2019-11-07 03:37:34 +0000

    removed hard coded database; implemented loadtweets; updated success callback

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -17,31 +17,31 @@
 //   },
 //   created_at: 1461116232227
 
-const data = [
-  {
-    user: {
-      name: 'Newton',
-      avatars: 'https://i.imgur.com/73hZDYK.png',
-      handle: '@SirIsaac'
-    },
-    content: {
-      text:
-        'If I have seen further it is by standing on the shoulders of giants'
-    },
-    created_at: 1461116232227
-  },
-  {
-    user: {
-      name: 'Descartes',
-      avatars: 'https://i.imgur.com/nlhLi3I.png',
-      handle: '@rd'
-    },
-    content: {
-      text: 'Je pense , donc je suis'
-    },
-    created_at: 1461113959088
-  }
-];
+// const data = [
+//   {
+//     user: {
+//       name: 'Newton',
+//       avatars: 'https://i.imgur.com/73hZDYK.png',
+//       handle: '@SirIsaac'
+//     },
+//     content: {
+//       text:
+//         'If I have seen further it is by standing on the shoulders of giants'
+//     },
+//     created_at: 1461116232227
+//   },
+//   {
+//     user: {
+//       name: 'Descartes',
+//       avatars: 'https://i.imgur.com/nlhLi3I.png',
+//       handle: '@rd'
+//     },
+//     content: {
+//       text: 'Je pense , donc je suis'
+//     },
+//     created_at: 1461113959088
+//   }
+// ];
 
 const renderTweets = function(tweets) {
   const $tweetContainer = $('.tweet-container');
@@ -51,6 +51,13 @@ const renderTweets = function(tweets) {
     // console.log(createTweetElement(tweet));
     const $tweet = createTweetElement(tweet);
     $tweetContainer.append($tweet);
+  });
+};
+
+const loadtweets = function() {
+  $.ajax('/tweets', { method: 'GET' }).then(function(data) {
+    console.log('Succuess', data);
+    renderTweets(data);
   });
 };
 
@@ -119,26 +126,26 @@ $(document).ready(function() {
   $('form').on('submit', function(e) {
     e.preventDefault();
     const tweet = $(this).serialize();
-    console.log('form b', $(this));
-    console.log('form submitted', tweet);
+    // console.log('form b', $(this));
+    // console.log('form submitted', tweet);
     $.ajax({
-      url: 'http://localhost:8080/tweets',
+      url: '/tweets',
       method: 'POST',
       dataType: 'json',
       data: tweet,
       success: function(data) {
+        console.log(this);
+        console.log(tweet);
         console.log('data is back :', data);
-        // data.forEach(post => {
-        //   $('.tweet-container').append(post);
-        // });
+        $('.tweet-container').prepend(createTweetElement(data));
       },
       error: function(err) {
         console.error('Error :', err);
       }
     });
-
-    renderTweets(data);
   });
+  loadtweets();
+  // renderTweets(data);
 });
 
 // $(document).ready(function() {
